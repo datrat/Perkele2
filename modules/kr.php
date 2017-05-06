9895a3c63c27436c31c8aecdaa5daa3ebeda4aac
datrat <moonfood@gmail.com>
2017-05-06 23:36:41 +0300

    added modules folder

---
+++ modules/kr.php
@@ -0,0 +1,14 @@
+<?php
+echo "hello2";
+echo "hello2";echo "hello";
+echo "hello2";
+echo "hello2";
+echo "hello2";
+echo "hello2";
+echo "hello2";echo "hello";
+echo "hello2";
+echo "hello";
+echo "hello";
+echo "hello";
+
+?>
